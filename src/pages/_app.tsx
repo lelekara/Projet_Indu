71dda9f5396de3aa0a650e6d1f8c64317e7b420c
lelekara <lelekara04147@gmail.com>
2023-11-10 13:51:02 +0100

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
+import { ChakraProvider } from '@chakra-ui/react'
+
+import "~/styles/globals.css";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <ChakraProvider>
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+    </ChakraProvider>
+  );
+};
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Link from "next/link";
+
+import styles from "./index.module.css";
+import SidebarWithHeader from "~/components/navbar";
+
+export default function Home() {
+  return (
+    <>
+    <SidebarWithHeader />
+    </>
+  );
+}

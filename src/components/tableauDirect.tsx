c05fad771bfa0824a3ae2208d007ca4ce09bcc9f
lelekara <lelekara04147@gmail.com>
2023-11-15 22:03:59 +0100

    Try fill tab

--- src/components/tableauDirect.tsx
+++ src/components/tableauDirect.tsx
@@ -1,43 +1,49 @@
-import { TableContainer, Table, TableCaption, Thead, Tr, Th, Tbody, Td, Tfoot } from "@chakra-ui/react";
+import { TableContainer, Table, TableCaption, Thead, Tr, Th, Tbody, Td, Tfoot, Switch } from "@chakra-ui/react";
+import { useState } from "react";
 
 
 export default function TabDirect() {
+
+  const [tag, setTag] = useState([])
+
+  function getTags() {
+    fetch("/api/getAllTags", {
+      method: "POST"
+      }).then(res => {
+        console.log(res.status);
+        res.json().then(Tag => {
+          console.log(tag)
+        })
+      })
+  
+
     return (
         <TableContainer>
   <Table variant='striped' colorScheme='teal'>
     <TableCaption>Imperial to metric conversion factors</TableCaption>
     <Thead>
       <Tr>
-        <Th>To convert</Th>
-        <Th>into</Th>
-        <Th isNumeric>multiply by</Th>
+        <Th>ID</Th>
+        <Th>Topic</Th>
+        <Th>Value</Th>
+        <Th>Last Seen</Th>
+        <Th>Visible ?</Th>
       </Tr>
     </Thead>
     <Tbody>
-      <Tr>
-        <Td>inches</Td>
-        <Td>millimetres (mm)</Td>
-        <Td isNumeric>25.4</Td>
-      </Tr>
-      <Tr>
-        <Td>feet</Td>
-        <Td>centimetres (cm)</Td>
-        <Td isNumeric>30.48</Td>
-      </Tr>
-      <Tr>
-        <Td>yards</Td>
-        <Td>metres (m)</Td>
-        <Td isNumeric>0.91444</Td>
-      </Tr>
+      {
+        tag.map((tag) => <>
+        <Tr key={tag.id}>
+          <Td>{tag.id}</Td>
+          <Td>{tag.topic}</Td>
+          <Td>{tag.value}</Td>
+          <Td>{tag.lastSeen}</Td>
+          <Td><Switch/></Td>
+        </Tr>
+      </>
+      )}
     </Tbody>
-    <Tfoot>
-      <Tr>
-        <Th>To convert</Th>
-        <Th>into</Th>
-        <Th isNumeric>multiply by</Th>
-      </Tr>
-    </Tfoot>
   </Table>
 </TableContainer>
-    )
+    )}
 }
---
+++ src/pages/api/broker.mjs
@@ -0,0 +1,33 @@
+// mqttClient.ts
+
+import mqtt, { MqttClient } from 'mqtt';
+import { db } from '~/server/db';
+
+const client = mqtt.connect("ws://helhatechniquecharleroi.xyz", {
+    port: 9001,
+    username: "groupe5",
+    password: "groupe5",
+})
+
+export default client;
+
+client.on('connect', () => {
+    console.log('connected');
+    client.subscribe('/groupe5/#')
+})
+
+client.on('message', async (topicMQTT, value) => {
+    console.log('connected');
+
+    const tag = await db.tag.upsert({
+        where: { topic: topicMQTT },
+    },
+        create: {
+        topic: topicMQTT,
+        value: value.toString(),
+    },
+        update: {
+        value: value.toString(),
+        lastSeen: new Date(),
+    })
+})
